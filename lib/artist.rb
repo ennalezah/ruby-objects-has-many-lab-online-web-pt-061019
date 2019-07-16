92aaf7c796a0e768152c5adcd3d4de02e367ef99
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-16 02:16:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist 
+  attr_accessor :name, :songs
+  @@collection = {}
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    song.artist = self
+  end
+  
+  def self.song_count
+   #collection = artist is key, songs array is value
+   
+  end
+end
