3c21d8e08811811d2069f39a3a7090b5bb052c18
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-16 02:40:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,11 @@ class Song
   end
   
   def artist_name 
-    if @artist == nil
-      nil
-    else 
-      self.artist = artist.name
-    end
+    # if @artist == nil
+    #   nil
+    # else 
+    #   self.artist = artist.name
+    # end
     
     # if @artist == nil ? nil : self.artist = artist.name
   end
