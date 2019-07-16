d4f5edfe693adbdd0645ced11f7da7b13ec029bd
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-16 02:37:59 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -12,10 +12,10 @@ class Post
   end
   
   def author_name
-    if @author == nil
-      nil
-    else
-      self.author = author.name
-    end
+    # if @author == nil
+    #   nil
+    # else
+    #   self.author = author.name
+    # end
   end
 end
